a27f3595523b78d9c04df2a5918d2e672cbbdf35
netop://ウィビ <paul@webb.page>
2026-06-19 00:33:09 -0700

    fixes url typo

--- Formula/disc.rb
+++ Formula/disc.rb
@@ -21,7 +21,7 @@
 class Disc < Formula
   desc "TypeScript-native, schema-first database (Gel/EdgeDB compatible)"
   homepage "https://disc.sh"
-  url "https://github.com/systemsoft/disc/archive/refs/tags/v2026.06.15.tar.gz"
+  url "https://github.com/systemsoft/disc/archive/refs/tags/v2026.06.18.tar.gz"
   version "2026.06.18"
   sha256 "ce0afb3db494c2ea68edfa07e77838141dbe949f4eb2b31827061c6c7c2b05ac"
   license "Apache-2.0"
