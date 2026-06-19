f2bea6f8cf97955b6e06b547949d8193477f36a7
netop://ウィビ <paul@webb.page>
2026-06-19 00:22:11 -0700

    fixes homebrew offense

--- Formula/disc.rb
+++ Formula/disc.rb
@@ -21,10 +21,8 @@
 class Disc < Formula
   desc "TypeScript-native, schema-first database (Gel/EdgeDB compatible)"
   homepage "https://disc.sh"
-  license "Apache-2.0"
-
-  # Stable: tagged release. Update on each new tag.
   url "https://github.com/systemsoft/disc/archive/refs/tags/v2026.06.15.tar.gz"
+  license "Apache-2.0"
   sha256 "ce0afb3db494c2ea68edfa07e77838141dbe949f4eb2b31827061c6c7c2b05ac"
   version "2026.06.18"
 
